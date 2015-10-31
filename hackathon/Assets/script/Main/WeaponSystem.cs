637ba6b8ede025ab7d94b43e410853c27705627b
Neves <nevesferreira.gabriel@hotmail.com>
2015-10-31 17:31:21 -0200

    partial finish scripts

--- hackathon/Assets/script/Main/WeaponSystem.cs
+++ hackathon/Assets/script/Main/WeaponSystem.cs
@@ -3,21 +3,93 @@ using System.Collections;
 
 public class WeaponSystem : MonoBehaviour {
 
+	public string weaponName = "gun";
+
 	public float damage;
+	private int damageLvl;
 	public float atackSpeed;
+	private int atackSpeedLvl; 
 	public float critChance;
+	private int critChanceLvl;
 	public GameObject projectile;
 
 	private GameObject enemy;
 
 	// Use this for initialization
 	void Start () {
+
+		damageLvl = PlayerPrefs.GetInt((weaponName + "dmg"));
+		atackSpeedLvl = PlayerPrefs.GetInt((weaponName + "speed"));
+		critChanceLvl = PlayerPrefs.GetInt((weaponName + "crit"));
+
+
+		//define the lvl and attribute relationship
+		damage = damage * damageLvl;
+		critChance = critChance * critChanceLvl;
+		atackSpeed = atackSpeed * atackSpeedLvl;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+
+	public int getDamageLvl(){
+
+		return PlayerPrefs.GetInt((weaponName + "dmg"));
+
+	}
+
+	public void setDamageLvl(int value){
+
+		PlayerPrefs.GetInt((weaponName + "dmg"));
+		damage = damage * damageLvl;
+
+
+	}
+
+	public int getAtackSpeedLvl(){
+		
+		return PlayerPrefs.GetInt((weaponName + "speed"));
+
+	}
+
+	public void setAtackSpeedLvl(int value){
+		
+		PlayerPrefs.SetInt((weaponName + "speed"),value);
+		atackSpeed = atackSpeed * atackSpeedLvl;
+
+	}
+	
+
+	public int getCritChanceLvl(){
+		
+		return PlayerPrefs.GetInt((weaponName + "crit"));
+
+	}
+
+	public void setCritChanceLvl(int value){
+		
+		PlayerPrefs.SetInt((weaponName + "crit"),value);
+		critChance = critChance * critChanceLvl;
+
+	}
+
+	public float getDamage(){
+		
+		return damage;
+	}
+
+	public float getCritChance(){
+		
+		return critChance;
+	}
+
+	public float getAtackSpeed(){
+		
+		return atackSpeed;
 	}
 
 
--- hackathon/Assets/script/Upgrade/UpgradeScreen.cs
+++ hackathon/Assets/script/Upgrade/UpgradeScreen.cs
@@ -4,6 +4,36 @@ using System.Collections;
 public class UpgradeScreen : MonoBehaviour {
 
 
+	private string weapon;
+	public GameObject[] weapons;
+	private GameObject selected;
+	private GameObject player;
+
+
+	private float	playerDamage;
+	private float playerDamageLvlCost;
+	
+	private float	playerAtackSpeed;
+	private float playerAtackSpeedLvlCost;
+	
+	private float	playerCritical;
+	private float playerCriticalLvlCost;
+
+
+	private int	weaponDamageLvl;
+	private float weaponDamageLvlCost;
+
+	private int	weaponAtackSpeedLvl;
+	private float weaponAtackSpeedLvlCost;
+
+	private int	weaponCriticalLvl;
+	private float weaponCriticalLvlCost;
+
+		
+	private float money;
+
+
+	
 
 
 
@@ -11,13 +41,150 @@ public class UpgradeScreen : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		weapon =  PlayerPrefs.GetString("equipped");
+		for (int i = 0; i < weapons.Length; i++) {
+
+			if(weapons[i].GetComponent<WeaponSystem>().weaponName == weapon){
+
+				selected = Instantiate(weapons[i]);
+				weaponDamageLvl = selected.GetComponent<WeaponSystem>().getDamageLvl();
+				weaponAtackSpeedLvl = selected.GetComponent<WeaponSystem>().getAtackSpeedLvl();
+				weaponCriticalLvl = selected.GetComponent<WeaponSystem>().getCritChanceLvl();
+
+			}
+		}
+		money = PlayerPrefs.GetFloat ("money", 0.0F);
+
+		playerAtackSpeed = PlayerPrefs.GetFloat("atackSpeed",1.0f);
+		playerDamage = PlayerPrefs.GetFloat("baseDamage",1.0f);
+		playerCritical = PlayerPrefs.GetFloat("criticalChance",1.5f);
+
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+//PLAYER UPGRADES
+
+	public void UpgradePlayerDamage(){
+		
+		if(money >= playerDamageLvlCost)
+		{
+			//increments damage
+			playerDamage++;
+			PlayerPrefs.SetFloat("baseDamage",playerDamage);
+			money = money - playerDamageLvlCost;
+			PlayerPrefs.SetFloat ("money", money);
+			
+		}
+		
+		//increase cost
+		playerDamageLvlCost = playerDamageLvlCost * playerDamage;
+		
+	}
+	
+	
+	
+	public void UpgradePlayerAtackSpeed(){
+		
+		if(money >= playerAtackSpeedLvlCost)
+		{
+			playerAtackSpeed++;
+			PlayerPrefs.SetFloat("atackSpeed",playerAtackSpeed);
+			money = money - playerAtackSpeedLvlCost;
+			PlayerPrefs.SetFloat ("money", money);
+			
+		}
+		
+		//increase cost
+		playerAtackSpeedLvlCost = playerAtackSpeedLvlCost * playerAtackSpeed;
+		
+	}
+	
+	
+	
+	
+
+	public void UpgradePlayerCritical(){
+		
+		if(money >= playerCriticalLvlCost)
+		{
+			playerCritical++;
+			money = money - playerCriticalLvlCost;
+			PlayerPrefs.SetFloat("criticalChance",playerCritical);
+			PlayerPrefs.SetFloat ("money", money);
+			
+		}
+		
+		//increase cost
+		playerCriticalLvlCost = playerCriticalLvlCost * playerCritical;
+		
+	}
+
+
+
+
+//WEAPON UPGRADES
+
+	public void UpgradeWeaponDamage(){
+
+		if(money >= weaponDamageLvlCost)
+		{
+			weaponDamageLvl++;
+			selected.GetComponent<WeaponSystem>().setDamageLvl(weaponDamageLvl);
+			money = money - weaponDamageLvlCost;
+			PlayerPrefs.SetFloat ("money", money);
+
+		}
+
+		//increase cost
+		weaponDamageLvlCost = weaponDamageLvlCost * weaponDamageLvl;
+
+	}
+
+
+
+	public void UpgradeWeaponAtackSpeed(){
+		
+		if(money >= weaponAtackSpeedLvlCost)
+		{
+			weaponAtackSpeedLvl++;
+			selected.GetComponent<WeaponSystem>().setDamageLvl(weaponAtackSpeedLvl);
+			money = money - weaponAtackSpeedLvlCost;
+			PlayerPrefs.SetFloat ("money", money);
+			
+		}
+		
+		//increase cost
+		weaponAtackSpeedLvlCost = weaponAtackSpeedLvlCost * weaponAtackSpeedLvl;
+		
+	}
+
+
+
+
+
+	public void UpgradeWeaponCritical(){
+		
+		if(money >= weaponCriticalLvlCost)
+		{
+			weaponCriticalLvl++;
+			selected.GetComponent<WeaponSystem>().setDamageLvl(weaponCriticalLvl);
+			money = money - weaponCriticalLvlCost;
+			PlayerPrefs.SetFloat ("money", money);
+			
+		}
+		
+		//increase cost
+		weaponCriticalLvlCost = weaponCriticalLvlCost * weaponCriticalLvl;
+		
+	}
+
+
+
 
 
 
