863fe30742b2e9bdab7e084e30be5f82fb758ff3
Neves <nevesferreira.gabriel@hotmail.com>
2015-11-01 01:09:01 -0200

    final first scene

---
+++ hackathon/Assets/Caps.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Caps : MonoBehaviour {
+
+	float caps = 0;
+	Text capsText;
+	GameObject behave;
+	
+	// Use this for initialization
+	void Start () {
+		
+		behave = GameObject.FindGameObjectWithTag ("Behaviour");
+		capsText = GetComponent<Text> ();
+		caps = behave.GetComponent<GameBehaviour> ().caps;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		caps = behave.GetComponent<GameBehaviour> ().caps;
+		capsText.text = caps.ToString();
+	}
+	
+
+
+}
